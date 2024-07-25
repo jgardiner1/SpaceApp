4e29c5e87403c9056b13b8809e8178f5746c5d11
jgardiner1 <jamesgardiner1@live.co.uk>
2024-07-25 02:47:17 +0100

    added SessionModel class and view to store and display session display cards

---
+++ SpaceApp/SpaceApp/MVVM/Model/SessionModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace SpaceApp.MVVM.Model
+{
+    internal class SessionModel
+    {
+        public string SessionName { get; set; }
+        public DateTime DateTime { get; set; }
+        public string Location { get; set; }
+        public string WeatherCondition { get; set; }
+        public string SkyCondition { get; set; }
+        public string[] Observables { get; set; }
+        public string ImageSource { get; set; }
+
+    }
+}
--- SpaceApp/SpaceApp/MVVM/ViewModel/SessionsViewModel.cs
+++ SpaceApp/SpaceApp/MVVM/ViewModel/SessionsViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SpaceApp.MVVM.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,41 @@ namespace SpaceApp.MVVM.ViewModel
 {
     internal class SessionsViewModel
     {
+
+        public ObservableCollection<SessionModel> Sessions { get; set; }
+
+        public SessionsViewModel()
+        {
+            Sessions = new ObservableCollection<SessionModel>();
+
+            Sessions.Add(new SessionModel
+            {
+                SessionName = "My SkyWatching Session",
+                DateTime = DateTime.Now,
+                Location = "Canvey Island",
+                WeatherCondition = "Clear",
+                SkyCondition = "Great",
+                Observables = new string[] { "Jupiter", "Mars", "Moon" },
+                ImageSource = "../../Images/Amateur Space Image.jpeg"
+            }) ;
+            Sessions.Add(new SessionModel
+            {
+                SessionName = "My SkyWatching Session",
+                DateTime = DateTime.Now,
+                Location = "Canvey Island",
+                WeatherCondition = "Clear",
+                SkyCondition = "Great",
+                Observables = new string[] { "Jupiter", "Mars", "Moon" }
+            });
+            Sessions.Add(new SessionModel
+            {
+                SessionName = "My SkyWatching Session",
+                DateTime = DateTime.Now,
+                Location = "Canvey Island",
+                WeatherCondition = "Clear",
+                SkyCondition = "Great",
+                Observables = new string[] { "Jupiter", "Mars", "Moon" }
+            });
+        }
     }
 }
