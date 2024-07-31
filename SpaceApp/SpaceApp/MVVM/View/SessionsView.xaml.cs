030dc8f9ccda49d768ee73cb8c33aa161c51e25b
jgardiner1 <jamesgardiner1@live.co.uk>
2024-07-31 19:01:17 +0100

    Polished styling of session cards and added ability to drag and drop to rearrange. Added button at top to add new sessions but not yet implemented

--- SpaceApp/SpaceApp/MVVM/View/SessionsView.xaml.cs
+++ SpaceApp/SpaceApp/MVVM/View/SessionsView.xaml.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SpaceApp.MVVM.ViewModel;
+using SpaceApp.MVVM.Model;
+using System;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace SpaceApp.MVVM.View
 {
@@ -20,9 +14,90 @@ namespace SpaceApp.MVVM.View
     /// </summary>
     public partial class SessionsView : UserControl
     {
+        private Point _dragStartPoint;
+        private SessionModel _draggedData;
         public SessionsView()
         {
             InitializeComponent();
+        }
+
+        private void OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // store mouse position
+            _dragStartPoint = e.GetPosition(null);
+        }
+
+        private void OnPreviewMouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                Point mousePos = e.GetPosition(null);
+                Vector diff = _dragStartPoint - mousePos;
+
+                // Start dragging if the mouse has moved far enough
+                if (Math.Abs(diff.X) > SystemParameters.MinimumHorizontalDragDistance ||
+                    Math.Abs(diff.Y) > SystemParameters.MinimumVerticalDragDistance)
+                {
+                    var item = FindAncestor<ContentPresenter>((DependencyObject)e.OriginalSource);
+                    if (item != null)
+                    {
+                        _draggedData = item.Content as SessionModel;
+                        if (_draggedData != null)
+                        {
+                            // Start the drag operation
+                            DragDrop.DoDragDrop(item, _draggedData, DragDropEffects.Move);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void OnDrop(object sender, DragEventArgs e)
+        {
+            if (_draggedData == null)
+                return;
+
+            var droppedData = e.Data.GetData(_draggedData.GetType()) as SessionModel;
+            var target = ((FrameworkElement)sender).DataContext as SessionModel;
+
+            var sessions = DataContext as SessionsViewModel;
+            int indexRemove = sessions.Sessions.IndexOf(_draggedData);
+            int indexInsert = sessions.Sessions.IndexOf(target);
+
+            if (indexRemove != indexInsert)
+            {
+                sessions.Sessions.RemoveAt(indexRemove);
+                sessions.Sessions.Insert(indexInsert, droppedData);
+            }
+
+            _draggedData = null;
+        }
+
+        private void OnDragEnter(object sender, DragEventArgs e)
+        {
+            if (!e.Data.GetDataPresent(_draggedData.GetType()) || sender == _draggedData)
+            {
+                e.Effects = DragDropEffects.None;
+            }
+        }
+
+        private void OnDragLeave(object sender, DragEventArgs e)
+        {
+            // Optional: Handle visual feedback on drag leave
+        }
+
+        // Utility method to find the ancestor of a specific type
+        private static T FindAncestor<T>(DependencyObject current) where T : DependencyObject
+        {
+            while (current != null)
+            {
+                if (current is T)
+                {
+                    return (T)current;
+                }
+                current = VisualTreeHelper.GetParent(current);
+            }
+            return null;
         }
     }
 }
--- SpaceApp/SpaceApp/MVVM/ViewModel/SessionsViewModel.cs
+++ SpaceApp/SpaceApp/MVVM/ViewModel/SessionsViewModel.cs
@@ -19,47 +19,49 @@ namespace SpaceApp.MVVM.ViewModel
 
             Sessions.Add(new SessionModel
             {
-                SessionName = "My SkyWatching Session",
+                SessionName = "SkyWatching Session 1",
                 DateTime = DateTime.Now,
                 Location = "Canvey Island",
                 WeatherCondition = "Clear",
                 SkyCondition = "Great",
-                Observables = new string[] { "Jupiter", "Mars", "Moon" },
+                Observables = new string[] { "Jupiter" },
                 ImageSource = "../../Images/Amateur Space Image.jpeg"
             }) ;
             Sessions.Add(new SessionModel
             {
-                SessionName = "My SkyWatching Session",
+                SessionName = "SkyWatching Session 2",
                 DateTime = DateTime.Now,
-                Location = "Canvey Island",
+                Location = "Basildon",
                 WeatherCondition = "Clear",
                 SkyCondition = "Great",
-                Observables = new string[] { "Jupiter", "Mars", "Moon" }
+                Observables = new string[] { "Moon, Venus" },
+                ImageSource = "../../Images/SpaceBackground1.jpeg"
             });
             Sessions.Add(new SessionModel
             {
-                SessionName = "My SkyWatching Session",
+                SessionName = "SkyWatching Session 3",
                 DateTime = DateTime.Now,
-                Location = "Canvey Island",
+                Location = "Scotland",
                 WeatherCondition = "Clear",
                 SkyCondition = "Great",
-                Observables = new string[] { "Jupiter", "Mars", "Moon" }
+                Observables = new string[] { "Mars, Moon, Milkyway" },
+                ImageSource = "../../Images/StarryNightSkyBG.png"
             });
             Sessions.Add(new SessionModel
             {
-                SessionName = "My SkyWatching Session",
+                SessionName = "SkyWatching Session 4",
                 DateTime = DateTime.Now,
-                Location = "Canvey Island",
-                WeatherCondition = "Clear",
+                Location = "France",
+                WeatherCondition = "Cloudy",
                 SkyCondition = "Great",
-                Observables = new string[] { "Jupiter", "Mars", "Moon" }
+                Observables = new string[] { "Nothing" }
             });
             Sessions.Add(new SessionModel
             {
-                SessionName = "My SkyWatching Session",
+                SessionName = "SkyWatching Session 5",
                 DateTime = DateTime.Now,
-                Location = "Canvey Island",
-                WeatherCondition = "Clear",
+                Location = "Belgium",
+                WeatherCondition = "Foggy",
                 SkyCondition = "Great",
                 Observables = new string[] { "Jupiter", "Mars", "Moon" }
             });
